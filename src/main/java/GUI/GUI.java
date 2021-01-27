df9c7910949782774682fc8d6d69ab1434fdbb5e
kin_darnakes <kindenene@gmail.com>
2021-01-27 19:18:36 +0100

    Change encoding to UTF-8

--- src/main/java/GUI/GUI.java
+++ src/main/java/GUI/GUI.java
@@ -13,44 +13,44 @@ public class GUI {
 
         while (!salir) {
 
-            System.out.println("");
+            System.out.println();
             System.out.println("---------------------------MENU-----------------------------");
-            System.out.println("1. Simular la lectura de dos sensores de la misma c·mara de"
-                    + "manera simult·nea.");
+            System.out.println("1. Simular la lectura de dos sensores de la misma c√°mara de"
+                    + "manera simult√°nea.");
             System.out.println("2. Simular la lectura de un sensor con valor superior a la temperatura "
-                    + "m·xima y una apertura de puerta simult·nea.");
-                    
-            System.out.println("3. Ingreso de una nueva c·mara frigorÌfica y lectura de los"
-                    + "sensores de temperatura de las c·maras restantes. ");
-            System.out.println("4. Resumen del estado de las c·maras.");
-            System.out.println("5. Salir");        
+                    + "m√°xima y una apertura de puerta simult√°nea.");
+
+            System.out.println("3. Ingreso de una nueva c√°mara frigor√≠fica y lectura de los"
+                    + "sensores de temperatura de las c√°maras restantes. ");
+            System.out.println("4. Resumen del estado de las c√°maras.");
+            System.out.println("5. Salir");
 
             try {
-                System.out.print("Indica una opciÛn a travÈs del dÌgito que se muestra:");
+                System.out.print("Indica una opci√≥n a trav√©s del d√≠gito que se muestra:");
                 opcion = sc.nextInt();
 
                 switch (opcion) {
                     case 1:
-                        //Llama al mÈtodo correspondiente.
+                        //Llama al m√©todo correspondiente.
                         break;
                     case 2:
-                        //Llama al mÈtodo correspondiente.
+                        //Llama al m√©todo correspondiente.
                         break;
                     case 3:
-                        //Llama al mÈtodo correspondiente.
+                        //Llama al m√©todo correspondiente.
                         break;
                     case 4:
-                        //Llama al mÈtodo correspondiente.
+                        //Llama al m√©todo correspondiente.
                         break;
                     case 5:
                         salir = true;
                         break;
                     default:
-                        System.out.println("Indique un dÌgito v·lido entre 1-5");
+                        System.out.println("Indique un d√≠gito v√°lido entre 1-5");
                 }
 
             } catch (InputMismatchException e) {
-                System.out.println("°Cuidado! Solo puedes insertar n˙meros.");
+                System.out.println("¬°Cuidado! Solo puedes insertar n√∫meros.");
                 sc.next();
             }
         }
