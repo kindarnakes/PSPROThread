50ccccfc9d07ca2d510a7ef4f4881c8fdab69442
kin_darnakes <kindenene@gmail.com>
2021-01-31 18:42:07 +0100

    arranque guard

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,25 @@
+import Controller.LogController;
+import Dao.ChamberDao;
+import GUI.GUI;
+import Model.Log;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+
 public class Main {
 
     public static void main(String[] args) {
+        //Evitamos que salga el log de Hibernate apagando los logs
+        LogManager.getLogManager().reset();
+        Logger globalLogger = Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
+        globalLogger.setLevel(java.util.logging.Level.OFF);
+
+        Log log = Log.getINSTANCE();
+        LogController logController = new LogController(log);
+        logController.start();
+        GUI.startGUI();
+        logController.shutdown();
 
     }
 }
