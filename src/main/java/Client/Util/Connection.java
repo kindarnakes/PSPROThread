56d8f0fc26503436cbad0bb6e5de52f68efb4f95
Carlos <arresu.ca@gmail.com>
2021-03-02 12:40:27 +0100

    Connnection

--- src/main/java/Client/Util/Connection.java
+++ src/main/java/Client/Util/Connection.java
@@ -15,32 +15,35 @@ public class Connection {
     DataOutputStream salida = null;
     String ipServidor = "127.0.0.1";
 
-    public void conectar(){
+    public void conectar() {
         try {
-        cliente = new Socket(ipServidor, 2019);
-        entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-        salida = new DataOutputStream(cliente.getOutputStream());
-    } catch (
-    UnknownHostException var9) {
-        System.err.println("El servidor no está levantado");
-    } catch (Exception var10) {
-        System.err.println("Error: " + var10);
+            cliente = new Socket(ipServidor, 2019);
+            entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+            salida = new DataOutputStream(cliente.getOutputStream());
+        } catch (
+                UnknownHostException var9) {
+            System.err.println("El servidor no está levantado");
+        } catch (Exception var10) {
+            System.err.println("Error: " + var10);
+        }
     }
 
-        try {
+        public void peticiones(){
+            try {
 
-        salida.close();
-        entrada.close();
-        cliente.close();
-    } catch (UnknownHostException var6) {
-        System.err.println("No encuentro el servidor en la dirección" + ipServidor);
-    } catch (
-    IOException var7) {
-        System.err.println("Error de entrada/salida");
-    } catch (Exception var8) {
-        System.err.println("Error: " + var8);
+                salida.close();
+                entrada.close();
+                cliente.close();
+            } catch (UnknownHostException var6) {
+                System.err.println("No encuentro el servidor en la dirección" + ipServidor);
+            } catch (
+                    IOException var7) {
+                System.err.println("Error de entrada/salida");
+            } catch (Exception var8) {
+                System.err.println("Error: " + var8);
+            }
+
+        }
     }
 
-}
-}
 
